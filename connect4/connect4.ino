a93741f5c5127a41d07bf3fc93a5d6929cbcef96
nummada <madalina.nuta1@gmail.com>
2021-05-17 00:18:46 +0300

    am desenat grid

--- connect4/connect4.ino
+++ connect4/connect4.ino
@@ -14,18 +14,35 @@ unsigned long center_button_last_debounce_time;
 unsigned long right_button_last_debounce_time;
 
 
-// lcd screen init with instance of the library
-#define cs   10
-#define dc   9
-#define rst  8
+// lcd screen defines with instance of the library
+#define cs 10
+#define dc 9
+#define rst 8
+#define width TFTscreen.width()
+#define height TFTscreen.height()
 TFT TFTscreen = TFT(cs, dc, rst);
 
+// rectangle variables
+#define LEFT_CORNER_X 20
+#define LEFT_CORNER_Y 30
+#define RECT_WIDTH (width - (LEFT_CORNER_X * 2))
+#define RECT_HEIGHT (height - (LEFT_CORNER_Y * 2) + 10)
+#define INTERVAL_HEIGHT (RECT_HEIGHT/6)
+#define INTERVAL_WIDTH (RECT_WIDTH/7)
 
+//game variables
+#define GAME_INIT 0
+#define GAME_BEGINS 1
+#define GAME_STARTED 2
+volatile byte game_state;
+byte player_turn;
+
+//setup the screen
 void setup_lcd_screen() {
   //initialize the library
   TFTscreen.begin();
 
-  // clear the screen with a black background
+  // clear the screen
   TFTscreen.background(0, 0, 0);
   //set the text size
   TFTscreen.setTextSize(2);
@@ -39,71 +56,84 @@ void setup() {
   // set interrupts for control buttons using PinChangeInt library
 
   // left button interrupt
-  pinMode(LEFT_BUTTON_PIN, INPUT_PULLUP);
-  PCintPort::attachInterrupt(LEFT_BUTTON_PIN, button_intrerrupt, RISING);
+  //pinMode(LEFT_BUTTON_PIN, INPUT_PULLUP);
 
-  // center button interrupt
-  pinMode(CENTER_BUTTON_PIN, INPUT_PULLUP);
-  PCintPort::attachInterrupt(CENTER_BUTTON_PIN, button_intrerrupt, RISING);
+  // center button interrupt -> the only one used in game setup
+  //pinMode(CENTER_BUTTON_PIN, INPUT_PULLUP);
+  PCintPort::attachInterrupt(CENTER_BUTTON_PIN, game_setup_interrupt, RISING);
 
   // right button interrupt
-  pinMode(RIGHT_BUTTON_PIN, INPUT_PULLUP);
-  PCintPort::attachInterrupt(RIGHT_BUTTON_PIN, button_intrerrupt, RISING);
+  //pinMode(RIGHT_BUTTON_PIN, INPUT_PULLUP);
+
+  // set pins 2,3,4 as input
+  DDRD = DDRD | B11100011;
 
   //setup screen
   setup_lcd_screen();
+
+  //game state
+  game_state = GAME_INIT;
+  player_turn = 0;
 }
 
+
+void draw_lines() {
+  for(int i = 1 ; i <= 5 ; i++) {
+    TFTscreen.line(LEFT_CORNER_X, LEFT_CORNER_Y + INTERVAL_HEIGHT * i, LEFT_CORNER_X + RECT_WIDTH, LEFT_CORNER_Y + INTERVAL_HEIGHT * i);
+  }
+
+  for(int i = 1 ; i <= 6 ; i++) {
+    TFTscreen.line(LEFT_CORNER_X + INTERVAL_WIDTH * i, LEFT_CORNER_Y, LEFT_CORNER_X + INTERVAL_WIDTH * i, LEFT_CORNER_Y + RECT_HEIGHT);
+  }
+}
 
 void loop() {
-  //generate a random color
-  int redRandom = random(0, 255);
-  int greenRandom = random (0, 255);
-  int blueRandom = random (0, 255);
-  
-  // set a random font color
-  TFTscreen.stroke(redRandom, greenRandom, blueRandom);
-  
-  // print Hello, World! in the middle of the screen
-  TFTscreen.text("Hello, World!", 6, 57);
+  // set a font color
+  TFTscreen.stroke(0, 0, 255);
+
+  if (game_state == GAME_INIT) {
+    TFTscreen.text("Press green", 15, 50);
+    TFTscreen.text("to play", 40, 80);
+  } else if (game_state == GAME_BEGINS) {
+    TFTscreen.background(0, 0, 0);
+    TFTscreen.rect(20, 30, RECT_WIDTH, RECT_HEIGHT);
+    TFTscreen.circle(0, 0, 2);
+    draw_lines();
+    game_state = GAME_STARTED;
+    PCintPort::detachInterrupt(CENTER_BUTTON_PIN);
+    PCintPort::attachInterrupt(CENTER_BUTTON_PIN, control_buttons_interrupt, RISING);
+    PCintPort::attachInterrupt(RIGHT_BUTTON_PIN, control_buttons_interrupt, RISING);
+    PCintPort::attachInterrupt(LEFT_BUTTON_PIN, control_buttons_interrupt, RISING);
+  }
 }
 
-void button_intrerrupt() {
+
+void control_buttons_interrupt() {
   if ((PIND & (1 << PD4)) != 0) {
-      if((long)(millis() - right_button_last_debounce_time) >= 150) {
+      if(millis() - right_button_last_debounce_time >= 150) {
         right_button_last_debounce_time = millis();
-        Serial.println("il vede pe 4");
+        Serial.println(4);
       }
   } else if ((PIND & (1 << PD3)) != 0) {
-      if((long)(millis() - center_button_last_debounce_time) >= 150) {
+      if(millis() - center_button_last_debounce_time >= 150) {
         center_button_last_debounce_time = millis();
-        Serial.println("il vede pe 3");
+        Serial.println(3);
       }
   } else if ((PIND & (1 << PD2)) != 0) {
-      if((long)(millis() - left_button_last_debounce_time) >= 150) {
+      if(millis() - left_button_last_debounce_time >= 150) {
         left_button_last_debounce_time = millis();
-        Serial.println("il vede pe 2");
+        Serial.println(2);
       }
   }
 }
 
-//void right_button_trigger() {
-//  if((long)(millis() - right_button_last_debounce_time) >= 150) {
-//    button_intrerrupt(RIGHT_BUTTON_PIN);
-//    right_button_last_debounce_time = millis();
-//  }
-//}
-//
-//void left_button_trigger() {
-//  if((long)(millis() - left_button_last_debounce_time) >= 150) {
-//    button_intrerrupt(LEFT_BUTTON_PIN);
-//    left_button_last_debounce_time = millis();
-//  }
-//}
-//
-//void center_button_trigger() {
-//  if((long)(millis() - center_button_last_debounce_time) >= 150) {
-//    button_intrerrupt(CENTER_BUTTON_PIN);
-//    center_button_last_debounce_time = millis();
-//  }
-//}
+void game_setup_interrupt() {
+  if ((PIND & (1 << PD3)) != 0) {
+      if(millis() - center_button_last_debounce_time >= 150) {
+        center_button_last_debounce_time = millis();
+        if (game_state == GAME_INIT) {
+            game_state = GAME_BEGINS;
+        }
+      }
+  }
+}
